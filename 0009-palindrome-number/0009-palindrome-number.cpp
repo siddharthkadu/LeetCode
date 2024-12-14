c2da179becfb0bbe92f865aab4864124c7bccf1a
siddhatth kadu <103137013+siddharthkadu@users.noreply.github.com>
2024-12-14 21:54:15 +0530

    Time: 0 ms (100%), Space: 8.7 MB (26.17%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-      
-      
-        string str=to_string(x);
-        
-        string str2=str;
-        int i=0;
-        int j=str2.size()-1;
-        while(i<j){
-            swap(str2[i],str2[j]);
-            i++;
-            j--;
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
         }
-        if(str2==str) {return true;}
-        else {return false;}
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+        return x == revertedNumber || x == revertedNumber / 10;
     }
 };
