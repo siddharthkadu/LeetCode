a6446a0af00142aff023f12100501c384a1a0a4a
siddharth kadu <103137013+siddharthkadu@users.noreply.github.com>
2025-02-03 20:34:31 +0530

    Time: 0 ms (100%), Space: 27.8 MB (64.71%) - LeetHub

---
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int longestMonotonicSubarray(vector<int>& nums) {
+    if (nums.empty()) return 0;
+
+    int inc = 1, dec = 1, maxLen = 1;
+
+    for (int i = 1; i < nums.size(); i++) {
+        if (nums[i] > nums[i - 1]) {
+            inc++;
+            dec = 1;
+        } else if (nums[i] < nums[i - 1]) {
+            dec++;
+            inc = 1;
+        } else {
+            inc = 1;
+            dec = 1;
+        }
+        maxLen = max(maxLen, max(inc, dec));
+    }
+
+    return maxLen;
+}
+};
