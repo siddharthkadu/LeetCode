d6377e98d00c6020fb7792acb63cb2f9b592999a
siddharth kadu <103137013+siddharthkadu@users.noreply.github.com>
2025-02-14 20:08:46 +0530

    Time: 22 ms (58.84%), Space: 78.6 MB (15.57%) - LeetHub

---
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
@@ -0,0 +1,24 @@
+class ProductOfNumbers {
+public:
+    vector<int> product={1};
+    int n=1;
+    ProductOfNumbers(){
+        product.reserve(40000);
+    }
+    
+    void add(int num) {
+        if (num==0) {
+            product={1};
+            n=1;
+        }
+        else{
+            product.push_back(product[n++-1]*num);
+        }
+    }
+    
+    int getProduct(int k) {
+        if (n<=k) return 0;
+        return product[n-1]/product[n-k-1];
+    }
+};
+
